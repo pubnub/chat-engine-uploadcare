561d1cfba21f77817cd6a960d85a0e9f26c9a34e
Dustin Raimondi <raimondi1337@gmail.com>
2017-09-22 13:21:49 -0700

    update example and instructions

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# Uploadcare Binding for PubNub Chat Engine
+# Uploadcare Plugin for Chat Engine
 
-Easily add image uploads to chatrooms using UploadCare online service.
+Adds the ability to upload images in ChatEngine.Chats via UploadCare service
+
+### Quick Start
+
+0. Have ChatEngine instantiated and connected, and have a channel you want to upload images on
+```javascript
+const CE = ChatEngineCore.create({
+    publishKey: 'pub-key-here',
+    subscribeKey: 'sub-key-here',
+});
+
+CE.connect('Username');
+CE.on('$.ready', () => { ... }
+```
+
+1. Attach this plugin to the channel you want, in this case global
+```javascript
+CE.global.plugin(ChatEngineCore.plugin['chat-engine-uploadcare']());
+```
+
+2. Bind UploadCare to a UI element
+```javascript
+const widget = uploadcare.Widget('[role=uploadcare-uploader]');
+CE.global.uploadcare.bind(widget);
+```
+
+3. Listen for the `$uploadcare.upload` events on your channel
+```javascript
+CE.global.on('$uploadcare.upload', (payload) => {
+    console.log(payload.data.cdnUrl);
+});
+```
---
+++ example/script.js
@@ -0,0 +1,74 @@
+// In this example we are going to create a chat client
+// With the uploadcare plugin to provide for uploading files
+
+//get some references to our UI elements
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+const widget = uploadcare.Widget('[role=uploadcare-uploader]');
+
+// get some references to functions
+let send = function () {};
+let submit = function () {};
+
+// create an instance of chat-engine
+const CE = ChatEngineCore.create({
+    publishKey: 'pub-c-bcf4e625-d5e0-45de-9f74-f222bf63a4a1',
+    subscribeKey: 'sub-c-70f29a7c-8927-11e7-af73-96e8309537a2',
+}, {
+    globalChannel: 'uploadcare-example'
+});
+
+//connect to the network
+CE.connect('George Costanza');
+
+// when the connection is ready, do some stuff
+CE.on('$.ready', () => {
+
+    // * * * * *  start plugin specific code  * * * * *
+
+    // attach the unread-messages plugin to the global channel
+    CE.global.plugin(ChatEngineCore.plugin['chat-engine-uploadcare']());
+
+    // bind the uploadcare plugin to it's UI element
+    CE.global.uploadcare.bind(widget);
+
+    // when an upload event is emitted on the global channel add an image to the chat log
+    CE.global.on('$uploadcare.upload', (payload) => {
+
+        $('#output').append($('<p><strong>'
+            + payload.sender.uuid + ':</strong><img src="'
+            + payload.data.cdnUrl+'"/></p>'));
+
+    });
+
+    // * * * * *  end plugin specific code  * * * * *
+
+    // when a message event is emitted on the global channel add text to the chat log
+    CE.global.on('message', (payload) => {
+
+        $('#output').append($('<p><strong>' + payload.sender.uuid + ':</strong> ' + payload.data.text + '</p>'));
+    
+    });
+
+    // use the input box value as message payload then clear it
+    send = function () {
+
+        CE.global.emit('message', {
+            text: input.value
+        });
+
+        input.value = '';
+
+        return false;
+
+    };
+
+    // hook up the enter key for maximum usability
+    submit = function (e) {
+
+        if (e.keyCode === 13) {
+            send();
+        }
+    };
+
+});
